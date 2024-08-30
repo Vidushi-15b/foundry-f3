b5435dec1ced5061b88afd168bd55f80b3337ac7
vidushi <vidushi61203@gmail.com>
2024-08-31 04:09:39 +0530

    second commit

---
+++ foundryf3/foundry-fund-me-f3/script/HelperConfig.s.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {MockV3Aggregator} from "../test/mocks/MockV3Aggregato.sol";
+import {Script, console2} from "forge-std/Script.sol";
+
+
+contract HelperConfig is Script {
+
+    NetworkConfig public activeNetworkConfig;
+    uint8 public constant DECIMALS = 8;
+    int256 public constant INITIAL_PRICE = 2000e8;
+
+    struct NetworkConfig {
+        address priceFeed;  
+    }
+
+    constructor() {
+        if (block.chainid == 11155111) {
+            activeNetworkConfig = getSepoliaEthConfig();
+        } 
+        
+        else {
+            activeNetworkConfig = getOrCreateAnvilEthConfig();
+        }
+        }
+    
+
+    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
+        NetworkConfig memory sepoliaConfig = NetworkConfig({priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306});
+    
+        return sepoliaConfig;
+        }
+          function getMainnetEthConfig() public pure returns (NetworkConfig memory) {
+        // Price feed address
+        NetworkConfig memory ethConfig = NetworkConfig({
+            priceFeed: 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419
+        });
+        return ethConfig;}  
+    function getOrCreateAnvilEthConfig() public returns (NetworkConfig memory) {
+        if (activeNetworkConfig.priceFeed != address(0)) {
+            return activeNetworkConfig;
+        } 
+        //if we've already deployed a price feed, just return that instead of deploying a new one
+        //else deploy one
+        
+        else {
+        vm.startBroadcast();
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(DECIMALS, INITIAL_PRICE);
+        vm.stopBroadcast();
+
+        NetworkConfig memory anvilConfig = NetworkConfig({priceFeed: address(mockPriceFeed)});
+        
+        return anvilConfig;}    
+        
+    }
+}
